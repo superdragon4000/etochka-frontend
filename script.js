229297641e145a117fcd0b5cc8df3d8543ba900c
lgizar ismakaev <ilgizar3332@gmail.com>
2022-10-18 18:31:28 +0300

    verstka

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ gameBtns.forEach((el) => {
     el.addEventListener('click', (e) => {
         console.log('sadas')
         e.target.disabled = true;
+        e.target.innerText = 'Уже использовано';
     })
 })
